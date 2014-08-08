5a18f473fccf3406364d5a97df3de473f51d76f9
mv2sa <marcosv.santanna@hotmail.com>
2014-08-07 21:57:56 -0400

    loads of changes
    add basic styles, base javascript, font awesome, got most of the stuff
    from santannaLaw.com

--- js/main.js
+++ js/main.js
@@ -1,1 +1,97 @@
+$(document).ready(function() {
+	$('#h-mobileButton').click(function(event) {
+		event.preventDefault();
+		GLOBALCONTROLS.mobileMenu();
+	});
+	$('#globalBackToTop a').click(function(event) {
+		event.preventDefault();
+		GLOBALCONTROLS.backToTop();
+	});
 
+	GLOBALCONTROLS.setPageWidth();
+
+	$(window).resize(function(){
+		GLOBALCONTROLS.setPageWidth();
+	});
+
+	$(window).scroll(function() {
+		GLOBALCONTROLS.getPageScroll();
+	});
+	
+});
+
+var GLOBALCONTROLS = {
+	pageWidth : window.innerWidth || document.documentElement.clientWidth,
+	pageScroll : 0,
+	isDesktop : false,
+	mobileScrollUp : false,
+	mobileMenu : function() {
+		var element = $('#h-mainMenu');
+		if(Modernizr.csstransitions === false) {
+			if(element.hasClass('active')) {
+				element.removeClass('active');
+				element.slideUp();
+			} else {
+				element.addClass('active');
+				element.slideDown();
+			}
+		} else {
+			if(element.hasClass('active')) {
+				element.removeClass('active');
+			} else {
+				element.addClass('active');
+			}
+		}
+	},
+	setPageWidth : function() {
+		var tempHolder;
+		tempHolder = GLOBALCONTROLS.isDesktop;
+		GLOBALCONTROLS.pageWidth = window.innerWidth || document.documentElement.clientWidth;
+		if (GLOBALCONTROLS.pageWidth > 767) {
+			GLOBALCONTROLS.isDesktop = true;
+			if (GLOBALCONTROLS.mobileScrollUp === true) {
+				GLOBALCONTROLS.mobileToTop(0);
+			}
+		} else {
+			GLOBALCONTROLS.isDesktop = false;
+		}
+		if (tempHolder !== GLOBALCONTROLS.isDesktop && GLOBALCONTROLS.isDesktop === true) {
+			$('.JQueryClear').attr('style', ' ').removeClass('active');
+		}
+	},
+	getPageScroll : function() {
+	    var scrollTop = 0;
+	    if (typeof(window.pageYOffset) === 'number') {
+	        scrollTop = window.pageYOffset;
+	        if ((scrollTop > 50 && GLOBALCONTROLS.pageScroll > scrollTop) && 
+	        (GLOBALCONTROLS.isDesktop === false && GLOBALCONTROLS.mobileScrollUp === false)) {
+	        	GLOBALCONTROLS.mobileToTop(1);
+	        } else if ((scrollTop > 50 && GLOBALCONTROLS.pageScroll < scrollTop) && 
+	        (GLOBALCONTROLS.isDesktop === false && GLOBALCONTROLS.mobileScrollUp === true)) {
+	        	GLOBALCONTROLS.mobileToTop(0);
+	        } else if (scrollTop <= 50 && (GLOBALCONTROLS.isDesktop === false && GLOBALCONTROLS.mobileScrollUp === true)) {
+	        	GLOBALCONTROLS.mobileToTop(0);
+	        }
+	    }
+	    $('#globalBackToTop').css('top', scrollTop + 5);
+	    GLOBALCONTROLS.pageScroll = scrollTop;
+	},
+	mobileToTop : function (state) {
+		if (state === 1) {
+			$('#globalBackToTop').fadeIn();
+			GLOBALCONTROLS.mobileScrollUp = true;
+		} else if (state === 0) {
+			$('#globalBackToTop').fadeOut();
+			GLOBALCONTROLS.mobileScrollUp = false;
+		}
+	},
+	backToTop : function() {
+		if (navigator.userAgent.match(/Windows Phone/i)) {
+			window.scrollTo(0, 0);
+		} else {
+			$("body,html").animate({
+			    scrollTop: 0
+			},500, "swing");		
+		}
+	}
+};
